17c99a29c0c2c36051a423476da9f368a535310f
nicoleivo <cucchiettinicolas@gmail.com>
2022-12-01 14:47:34 +0100

    path change for deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import { persistStore } from 'redux-persist';
 import { BrowserRouter as Router } from 'react-router-dom';
 
